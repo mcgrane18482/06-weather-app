40a21dc496b43f4da92484ee5c7f0b392f765f1e
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-06-18 20:04:13 -0400

    added logic for retrieving and displaying data from the API

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
 var historyOutput = $("#searched-city")
 var searchBtn = $("#submit-button");
 var searchInput = $('#search-input');
-
+var cityNameOutput = $('#city-name-text')
+var weatherData = $('#weather-data')
+var fiveDayData = $('#five-day-output')
 var apiKey = '711591ebf940ef8fe2730bd1f7f8e0a1'
-var baseCurrentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial'
+
 var baseGeoURL = 'http://api.openweathermap.org/geo/1.0/zip?'
 var lat;
 var lon;
@@ -21,12 +23,36 @@ function getLocalStorage() {
 
 searchBtn.on("click", saveSearch);
 
-function getCity() {
-    var zipCode = searchInput.val();
-    searchInput.val('')
-    var geoURL = baseGeoURL + `zip=${zipCode},US&limit=4&appid=${apiKey}`
-    console.log(geoURL)
+function getCity(cityName) {
+    var URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
+    fetch(URL).then(function (response){
+        return response.json()
+    }).then(function (currentData){
+        console.log(currentData)
+        cityNameOutput.html(`${currentData.name} ${dayjs.unix(currentData.dt).format("MM/DD/YYYY")}<img src="https://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png">`)
+        weatherData.html(`<p><span class = "is-bold">Temp:</span> ${currentData.main.temp}</p>
+        <p><span class = "is-bold">Wind: </span> ${currentData.wind.speed}</p>
+        <p><span class = "is-bold">Humidity: </span> ${currentData.main.humidity}</p>`)
+    })
+    var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
+    fetch(fiveDayURL).then(function (res){
+        return res.json()
+    }).then(function (forecastData){
+        console.log(forecastData)
+        fiveDayData.empty()
+        for (var i=4; i< forecastData.list.length; i+=8){
+            console.log(forecastData.list[i])
+            fiveDayData.append(`<div class ="card col weather-card p-2 mx-1">
+            <h4>${dayjs.unix(forecastData.list[i].dt).format("MM/DD/YYYY")}</h4>
+            <img src="https://openweathermap.org/img/wn/${forecastData.list[i].weather[0].icon}@2x.png">
+            <p>Temp:${forecastData.list[i].main.temp}</p>
+            <p>Wind: ${forecastData.list[i].wind.speed}</p>
+            <p>Humidity: ${forecastData.list[i].main.humidity}</p>
+        </div>`)
+        }
+    })
 }
+
 
 function saveSearch() {
     var searchHistory = getLocalStorage();
@@ -34,7 +60,7 @@ function saveSearch() {
         var city = searchInput.val();
         searchHistory.push(city)
         localStorage.setItem('cities', JSON.stringify(searchHistory));
-        getCity();
+        getCity(city);
         displayHistory();
     }
     else {
@@ -42,9 +68,9 @@ function saveSearch() {
     }
 }
 
-// function fetchWeather(){
-
-// }
+function fetchWeather(){
+    getCity($(this).text())
+}
 
 function displayHistory() {
     var parsed = JSON.parse(localStorage.getItem('cities'));
@@ -53,5 +79,5 @@ function displayHistory() {
         historyOutput.prepend(`<li class ="list-group-item">${items}</li>`);
     }
     $("historyOutput:last-child").attr('id', 'last-search-item');
+    $("#searched-city").on('click', fetchWeather)
 }
-
