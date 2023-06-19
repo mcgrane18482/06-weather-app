ed4c633cbe0dc57368d1167beb606f7b77b5e554
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-06-18 21:05:15 -0400

    fixed the logic for fetchWeather function and made minor CSS adjustments

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,10 @@
-var historyOutput = $("#searched-city")
+var historyOutput = $("#history-display")
 var searchBtn = $("#submit-button");
 var searchInput = $('#search-input');
 var cityNameOutput = $('#city-name-text')
 var weatherData = $('#weather-data')
 var fiveDayData = $('#five-day-output')
 var apiKey = '711591ebf940ef8fe2730bd1f7f8e0a1'
-
-var baseGeoURL = 'http://api.openweathermap.org/geo/1.0/zip?'
-var lat;
-var lon;
-
 
 if (localStorage.getItem('cities')) {
     displayHistory();
@@ -21,16 +16,14 @@ function getLocalStorage() {
     return parsed || [];
 }
 
-searchBtn.on("click", saveSearch);
-
-function getCity(cityName) {
+function getForecast(cityName) {
     var URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
     fetch(URL).then(function (response){
         return response.json()
     }).then(function (currentData){
         console.log(currentData)
         cityNameOutput.html(`${currentData.name} ${dayjs.unix(currentData.dt).format("MM/DD/YYYY")}<img src="https://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png">`)
-        weatherData.html(`<p><span class = "is-bold">Temp:</span> ${currentData.main.temp}</p>
+        weatherData.html(`<p><span class = "is-bold">Temp: </span> ${currentData.main.temp}</p>
         <p><span class = "is-bold">Wind: </span> ${currentData.wind.speed}</p>
         <p><span class = "is-bold">Humidity: </span> ${currentData.main.humidity}</p>`)
     })
@@ -45,7 +38,7 @@ function getCity(cityName) {
             fiveDayData.append(`<div class ="card col weather-card p-2 mx-1">
             <h4>${dayjs.unix(forecastData.list[i].dt).format("MM/DD/YYYY")}</h4>
             <img src="https://openweathermap.org/img/wn/${forecastData.list[i].weather[0].icon}@2x.png">
-            <p>Temp:${forecastData.list[i].main.temp}</p>
+            <p>Temp: ${forecastData.list[i].main.temp}</p>
             <p>Wind: ${forecastData.list[i].wind.speed}</p>
             <p>Humidity: ${forecastData.list[i].main.humidity}</p>
         </div>`)
@@ -53,14 +46,13 @@ function getCity(cityName) {
     })
 }
 
-
 function saveSearch() {
     var searchHistory = getLocalStorage();
     if (searchInput.val()) {
         var city = searchInput.val();
         searchHistory.push(city)
         localStorage.setItem('cities', JSON.stringify(searchHistory));
-        getCity(city);
+        getForecast(city);
         displayHistory();
     }
     else {
@@ -68,16 +60,18 @@ function saveSearch() {
     }
 }
 
-function fetchWeather(){
-    getCity($(this).text())
+function fetchWeather(event){
+    var clicked = event.target
+    getForecast(clicked.innerText)
 }
 
 function displayHistory() {
     var parsed = JSON.parse(localStorage.getItem('cities'));
     historyOutput.text('')
     for (var items of parsed) {
-        historyOutput.prepend(`<li class ="list-group-item">${items}</li>`);
+        historyOutput.prepend(`<button class ="btn btn-light my-1">${items}</button>`);
     }
-    $("historyOutput:last-child").attr('id', 'last-search-item');
-    $("#searched-city").on('click', fetchWeather)
+    historyOutput.on('click', fetchWeather)
 }
+
+searchBtn.on("click", saveSearch);
